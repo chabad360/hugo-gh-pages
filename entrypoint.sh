26d507ec5e3681e4cce1c623d5a6e842bb7105a3
chabad360 <chabad360@users.noreply.github.com>
2019-09-27 18:17:07 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,8 @@ echo "Setting up git"
 [ -z "${BRANCH}" ] && \
   (BRANCH=gh-pages)
 
-git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 echo "machine github.com login ${GITHUB_ACTOR} password ${GITHUB_TOKEN}" > ~/.netrc
 
 echo "cloning"
