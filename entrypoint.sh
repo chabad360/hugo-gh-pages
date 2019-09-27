fff915686d995481bcaa931963c4ef8b1e8be671
chabad360 <chabad360@users.noreply.github.com>
2019-09-27 18:10:28 -0400

    Adding custom branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,12 +8,15 @@ hugo "$@"
 echo "Setting up git"
 [ -z "${GITHUB_TOKEN}" ] && \
   (echo "ERROR: Missing GITHUB_TOKEN." ; exit 1)
-git config --global user.name "${GITHUB_ACTOR}"
-git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+[ -z "${BRANCH}" ] && \
+  (BRANCH=gh-pages)
+
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 echo "machine github.com login ${GITHUB_ACTOR} password ${GITHUB_TOKEN}" > ~/.netrc
 
 echo "cloning"
-git clone --depth=1 --single-branch --branch gh-pages https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git /tmp/gh-pages
+git clone --depth=1 --single-branch --branch ${BRANCH} https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git /tmp/gh-pages
 
 echo "copying"
 rm -rf /tmp/gh-pages/*
@@ -21,11 +24,7 @@ cp -av public/* /tmp/gh-pages/
 
 echo "commit & push"
 cd /tmp/gh-pages
-git add -A && git commit --allow-empty -am "Publishing from mattbailey/actions-hugo ${GITHUB_SHA}"
-git push
+git add -A && git commit --allow-empty -am "Publishing Site at ${GITHUB_SHA} Time: $(date -u)"
+git push --force
 
-#echo "Triggering second commit (for some gh-pages builds, two commits are required)."
-#date -u > last_deploy.txt
-#git add last_deploy.txt
-#git commit -am "Publishing from mattbailey/actions-hugo ${GITHUB_SHA}"
-#git push
+echo "${GITHUB_SHA} was successfully deployed"
