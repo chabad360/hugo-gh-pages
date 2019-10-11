232949b3869fff336a8ddd7bccef75c5e363f7ef
Mendel Greenberg <chabad360@gmail.com>
2019-10-11 15:20:26 -0700

    Minor changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,25 +2,36 @@
 
 set -e
 
-hugo version
-pygmentize
+if [ "${CNAME}" ]; then
+  NAME=${CNAME}
+else
+  NAME=${GITHUB_REPOSITORY}
+fi
 
-echo "Generating Site"
-hugo "$@"
-
-echo "Setting up Git"
 [ -z "${GITHUB_TOKEN}" ] && \
   (echo "ERROR: Missing GITHUB_TOKEN." ; exit 1)
+
 [ -z "${BRANCH}" ] && \
   (BRANCH=gh-pages)
 
+echo "Versions"
+hugo version
+pygmentize -V
+
+
+echo "Generating Site ${NAME} at commit ${GITHUB_SHA}"
+hugo "$@"
+
+
+echo "Setting up Git"
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 echo "machine github.com login ${GITHUB_ACTOR} password ${GITHUB_TOKEN}" > ~/.netrc
 
 git clone --depth=1 --single-branch --branch ${BRANCH} https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git /tmp/gh-pages
 
-echo "Copying"
+
+echo "Commiting"
 rm -rf /tmp/gh-pages/*
 cp -a public/* /tmp/gh-pages/
 cd /tmp/gh-pages
@@ -28,8 +39,10 @@ cd /tmp/gh-pages
 [ "${CNAME}" ] && \
   echo "${CNAME}" > CNAME
 
+git add -A && git commit --allow-empty -am "Publishing Site ${NAME} at ${GITHUB_SHA} on $(date -u)"
+
 echo "Push"
-git add -A && git commit --allow-empty -am "Publishing Site at ${GITHUB_SHA} on $(date -u)"
 git push --force
 
-echo "${GITHUB_SHA} was successfully deployed!"
+
+echo "Site ${NAME} at ${GITHUB_SHA} was successfully deployed!"
