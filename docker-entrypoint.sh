bfd5d2fa3abefe9ac3837c6aa9b685122d42feda
chabad360 <chabad360@gmail.com>
2020-03-03 01:12:21 -0500

    explicitly untar hugo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,9 +9,9 @@ BOLD='\033[1;37m'
 if [ "${INPUT_HUGOVERSION}" ]; then
   echo -e "\n${BOLD}Using Hugo version ${INPUT_HUGOVERSION}.${PLAIN}"
   wget "https://github.com/gohugoio/hugo/releases/download/v$(echo "${INPUT_HUGOVERSION}" | grep -o  "[0-9]\+.[0-9]\+.[0-9]\+")/hugo_${INPUT_HUGOVERSION}_Linux-64bit.tar.gz"
-  tar x -zvf hugo*
+  tar x -zvf hugo*.tar.gz
   mv hugo /usr/bin/hugo
-  rm hugo*
+  rm hugo*.tar.gz
 fi
 
 
