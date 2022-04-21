28730ede8ba253b4460497ab9110bb5b6f5def4c
Rafael Fernández López <ereslibre@ereslibre.es>
2022-04-21 12:10:15 +0300

    Add `$GITHUB_WORKSPACE` as a safe directory
    In the context of a GitHub action, mark `$GITHUB_WORKSPACE` as a safe
    directory.
    
    Related:
    - https://github.blog/2022-04-12-git-security-vulnerability-announced/
    - https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2022-24765

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -51,6 +51,7 @@ babel --version
 echo -e "\n${BOLD}Setting up Git${PLAIN}"
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git config --global --add safe.directory "${GITHUB_WORKSPACE}"
 echo "machine github.com login ${GITHUB_ACTOR} password ${INPUT_GITHUBTOKEN}" > ~/.netrc
 
 git clone --depth=1 --single-branch --branch "${INPUT_BRANCH}" "https://x-access-token:${INPUT_GITHUBTOKEN}@github.com/${REPO}.git" /tmp/gh-pages
